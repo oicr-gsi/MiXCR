0faf03c69dbc64ae8dae7bb61f75fc01db57ed5d
alkabenawra <alkabenawra@rogers.com>
2018-06-20 10:12:54 -0400

    test

--- workflow-mixcr/src/main/java/ca/on/oicr/pde/workflows/MiXCRWorkflowClient.java
+++ workflow-mixcr/src/main/java/ca/on/oicr/pde/workflows/MiXCRWorkflowClient.java
@@ -186,7 +186,7 @@ public class MiXCRWorkflowClient extends OicrWorkflow {
     private Job alignVDJCgenes() {
         Job VDJCgenes = getWorkflow().createBashJob("VDJCgenes");
         Command cmd = VDJCgenes.getCommand();
-        cmd.addArgument("module load java/1.8.0_91");
+        cmd.addArgument("module load java/1.8.0_91");    
         cmd.addArgument("which java");
         cmd.addArgument("echo $javahome");
         cmd.addArgument("echo $MODULEPATH");
